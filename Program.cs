3e06421e9aad649fae9e61bc855c23a19ce169fd
YuriyP <solovej-1991@mail.ru>
2022-06-24 00:42:39 +0600

    Commit 1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
-﻿Console.Write("Input number: "); // запрашиваем пользователя число
+﻿Console.Write("Input number: "); // запрашиваем пользователя 1 число
 int number_1 = Convert.ToInt32(Console.ReadLine()); //преобразовывает из консоли в целое число
 
-Console.WriteLine(number_1 + "->" + number_1*number_1); //производим всю операцию по возведению числа в квадрат
+Console.Write("Input number: "); // запрашиваем пользователя 2 число
+int number_2 = Convert.ToInt32(Console.ReadLine()); //преобразовывает из консоли в целое число
+
+if (number_1 == number_2*number_2)
+    Console.WriteLine("a = " + number_1 + ", b = " + number_2 + " ->" + " yes");
+else
+    Console.WriteLine("a = " + number_1 + ", b = " + number_2 + " ->" + " no");
